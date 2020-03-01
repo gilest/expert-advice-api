97224f88e1e040eefba43eb0bdf6362ee188f1e1
Giles Thompson <giles@papaiti.com>
2020-03-02 11:18:32 +1300

    App-wide: renamed project to Expert Advice

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -52,7 +52,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "starter_project_#{Rails.env}"
+  # config.active_job.queue_name_prefix = "expert_advice_#{Rails.env}"
 
   config.action_mailer.perform_caching = false
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,2 @@
 Doorkeeper::Application.destroy_all
-Doorkeeper::Application.create(name: "starter_project_web", redirect_uri: "http://localhost:3000/")
+Doorkeeper::Application.create(name: "expert_advice_web", redirect_uri: "http://localhost:3000/")
