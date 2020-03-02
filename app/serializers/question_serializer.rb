fa8eaea871c6eb2a76982d7959ec2cede1cb59d1
Giles Thompson <giles@papaiti.com>
2020-03-02 16:24:15 +1300

    Bug: do not serialise question id, only slug
    Avoiding conflicts in ember-data's identity map.

--- app/serializers/question_serializer.rb
+++ app/serializers/question_serializer.rb
@@ -1,5 +1,9 @@
 class QuestionSerializer < ActiveModel::Serializer
-  attributes :id, :user, :title, :description, :tags, :views, :slug
+  attributes :id, :user, :title, :description, :tags, :views
+
+  def id
+    object.slug
+  end
 
   def tags
     object.tags.split(',')
