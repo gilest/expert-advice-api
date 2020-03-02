dbcee3a7a3a7ea55a97cbb2bc465a0677380cbfe
Giles Thompson <giles@papaiti.com>
2020-03-02 14:21:03 +1300

    Seeds: added additional seed questions

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,7 @@
+def random_views
+  (rand() * 10_000).round(0)
+end
+
 ActiveRecord::Base.transaction do
   Doorkeeper::Application.destroy_all
   Doorkeeper::Application.create(name: "expert_advice_web", redirect_uri: "http://localhost:3000/")
@@ -12,8 +16,51 @@ ActiveRecord::Base.transaction do
 
   Question.create(
     user: user,
+    title: "How do I vet a potential employee?",
+    description: "Vetting is the process of performing a background check on someone before offering them employment, conferring an award, or doing fact-checking prior to making any decision. In addition, in intelligence gathering, assets are vetted to determine their usefulness.",
+    tags: "employment,procurement,business,risk",
+    views: random_views,
+    created_at: Time.now - 1.hour,
+    updated_at: Time.now - 1.hour
+  )
+
+  Question.create(
+    user: user,
     title: "How many hours of sleep should I aim for?",
     description: "The title says it all, really.",
-    tags: "sleep,work,health,lifestyle"
+    tags: "sleep,work,health,lifestyle",
+    views: random_views
+  )
+
+  Question.create(
+    user: user,
+    title: "How is life as a web developer who works remotely?",
+    description: "The title says it all, really.",
+    tags: "work,health,lifestyle,remote",
+    views: random_views
+  )
+
+  Question.create(
+    user: user,
+    title: "What's a single line of code that almost caused a catastrophe?",
+    description: "The title says it all, really.",
+    tags: "code,failure,safety",
+    views: random_views
+  )
+
+  Question.create(
+    user: user,
+    title: "Why do most top Computer Science students choose to become a backend developer instead of frontend developers?",
+    description: "The title says it all, really.",
+    tags: "education,web development,front end, back end",
+    views: random_views
+  )
+
+  Question.create(
+    user: user,
+    title: "What are the dirty little secrets of a back-end developer?",
+    description: "The title says it all, really.",
+    tags: "back end,web development,secrets",
+    views: random_views
   )
 end
