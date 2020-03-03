c4cf14b5bcc8d492af1dcf64ec4bf123d153587b
Giles Thompson <giles@papaiti.com>
2020-03-03 14:30:11 +1300

    Bugfix: validate that Question has a user

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,7 +2,7 @@ class Question < ApplicationRecord
   belongs_to :user
   has_many :answers
 
-  validates :title, :description, presence: true
+  validates :user, :title, :description, presence: true
 
   extend FriendlyId
   friendly_id :title, use: :slugged
